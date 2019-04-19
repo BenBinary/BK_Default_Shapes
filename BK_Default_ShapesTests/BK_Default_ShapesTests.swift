4e056dae2018640a56505517c44beece24c32801
Bene <bk191414@gmail.com>
2019-04-19 09:09:17 +0200

    Initial Commit

---
+++ BK_Default_ShapesTests/BK_Default_ShapesTests.swift
@@ -0,0 +1,34 @@
+//
+//  BK_Default_ShapesTests.swift
+//  BK_Default_ShapesTests
+//
+//  Created by Benedikt Kurz on 19.04.19.
+//  Copyright © 2019 Benedikt Kurz. All rights reserved.
+//
+
+import XCTest
+@testable import BK_Default_Shapes
+
+class BK_Default_ShapesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
